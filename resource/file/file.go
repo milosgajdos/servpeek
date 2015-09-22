6b451485af0499c62b7a6ccce2be4439aae3ffb6
Milos Gajdos <milosgajdos83@gmail.com>
2015-09-22 23:51:23 +0100

    Removed unnecessary functions implemented content check.

--- resource/file/file.go
+++ resource/file/file.go
@@ -1,17 +1,27 @@
 package file
 
 import (
+	"bufio"
 	"crypto/md5"
 	"crypto/sha256"
 	"encoding/hex"
 	"io"
 	"os"
+	"os/user"
 	"regexp"
+	"strconv"
 	"syscall"
 	"time"
-
-	"github.com/milosgajdos83/servpeek/utils"
 )
+
+func withOsFileContext(path string, fn func(file *os.File) (bool, error)) (bool, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return false, err
+	}
+	defer file.Close()
+	return fn(file)
+}
 
 type File struct {
 	fi os.FileInfo
@@ -56,38 +66,27 @@ func (f *File) IsSymlink() bool {
 	return f.fi.Mode()&os.ModeSymlink != 0
 }
 
-func (f *File) Contains(content regexp.Regexp) bool {
-	return false
-}
-
 func (f *File) IsMode(mode os.FileMode) bool {
 	return f.fi.Mode()&mode != 0
 }
 
-func (f *File) IsOwnedBy(user string) (bool, error) {
-	uid, err := utils.Uid(user)
+func (f *File) IsOwnedBy(username string) (bool, error) {
+	u, err := user.Lookup(username)
 	if err != nil {
 		return false, err
 	}
-	return f.fi.Sys().(*syscall.Stat_t).Uid == uid, nil
+	uid, err := strconv.ParseUint(u.Uid, 10, 32)
+	if err != nil {
+		return false, err
+	}
+	return f.fi.Sys().(*syscall.Stat_t).Uid == uint32(uid), nil
 }
 
-// FIXME: Golang can't do groupname lookups :-/
+// FIXME: Golang can't do full user gids lookups :-/
 // https://github.com/golang/go/issues/2617
+// For the time being you can only match against gid
 func (f *File) IsGrupedInto(gid uint32) bool {
 	return f.fi.Sys().(*syscall.Stat_t).Gid == gid
-}
-
-func (f *File) IsReadableBy(username string) bool {
-	return false
-}
-
-func (f *File) IsWritableBy(username string) bool {
-	return false
-}
-
-func (f *File) IsExecutableBy(username string) bool {
-	return false
 }
 
 func (f *File) LinksTo(path string) (bool, error) {
@@ -98,17 +97,8 @@ func (f *File) LinksTo(path string) (bool, error) {
 	return true, nil
 }
 
-func withFileContext(path string, fn func(file *os.File) (bool, error)) (bool, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return false, err
-	}
-	defer file.Close()
-	return fn(file)
-}
-
 func (f *File) Md5(sum string) (bool, error) {
-	return withFileContext(f.fi.Name(), func(file *os.File) (bool, error) {
+	return withOsFileContext(f.fi.Name(), func(file *os.File) (bool, error) {
 		hasher := md5.New()
 		if _, err := io.Copy(hasher, file); err != nil {
 			return false, nil
@@ -118,7 +108,7 @@ func (f *File) Md5(sum string) (bool, error) {
 }
 
 func (f *File) Sha256(sum string) (bool, error) {
-	return withFileContext(f.fi.Name(), func(file *os.File) (bool, error) {
+	return withOsFileContext(f.fi.Name(), func(file *os.File) (bool, error) {
 		hasher := sha256.New()
 		if _, err := io.Copy(hasher, file); err != nil {
 			return false, nil
@@ -133,4 +123,16 @@ func (f *File) Size(size int64) bool {
 
 func (f *File) ModTimeAfter(mtime time.Time) bool {
 	return f.fi.ModTime().After(mtime)
+}
+
+func (f *File) Contains(content regexp.Regexp) (bool, error) {
+	return withOsFileContext(f.fi.Name(), func(file *os.File) (bool, error) {
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			if content.Match(scanner.Bytes()) {
+				return true, nil
+			}
+		}
+		return false, nil
+	})
 }
