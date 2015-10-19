071113d668e40a183e88d9b8dda2f5d56ba60f39
Milos Gajdos <milosgajdos83@gmail.com>
2015-10-19 03:08:43 +0100

    Refactored the original quickly hacked code.
    withDockerAPI[Image|Container] functions should hopefully help avoid
    code duplications in many potential matchers that will be added later
    on.

--- resource/container/docker.go
+++ resource/container/docker.go
@@ -9,63 +9,119 @@ import (
 	"github.com/milosgajdos83/servpeek/resource"
 )
 
-// creates docker client and calls whatever func with it
-func withDockerClient(fn func(*docker.Client) error) error {
-	var err error
-	var dockerEndPoint string
-	// Check if DOCKER_HOST env var is set
+// Reads DOCKER_HOST environment variable
+// If it's empty, it validates and returns default docker endpoint
+func getDockerEndPoint() (dep string, err error) {
 	dockerHost := os.Getenv("DOCKER_HOST")
 	if dockerHost != "" {
-		dockerEndPoint = dockerHost
+		dep = dockerHost
 	} else {
-		dockerEndPoint, err = docker.DefaultDockerHost()
+		dep, err = docker.DefaultDockerHost()
 		if err != nil {
-			return err
+			return
 		}
 	}
-	// Check if DOCKER_CERT_PATH is set
+	return
+}
+
+// Reads DOCKER_CERT_PATH environment variable and returns a slice of strings
+// that contains TLS ca, cert and key
+func getDockerTLSCertPaths() []string {
 	dockerCertPath := os.Getenv("DOCKER_CERT_PATH")
 	if dockerCertPath != "" {
 		ca := fmt.Sprintf("%s/ca.pem", dockerCertPath)
 		cert := fmt.Sprintf("%s/cert.pem", dockerCertPath)
 		key := fmt.Sprintf("%s/key.pem", dockerCertPath)
-		client, err := docker.NewTLSClient(dockerEndPoint, cert, key, ca)
+		return []string{ca, cert, key}
+	}
+	return nil
+}
+
+// Creates docker client and calls an anonymous func passed as argument
+// If DOCKER_CERT_PATH is set, it creates HTTPS client, otherwise defaults to HTTP
+func withDockerClient(fn func(*docker.Client) error) error {
+	dockerEP, err := getDockerEndPoint()
+	if err != nil {
+		return err
+	}
+	dTLSCerPaths := getDockerTLSCertPaths()
+	// Use HTTPS client
+	if len(dTLSCerPaths) == 3 {
+		ca, cert, key := dTLSCerPaths[0], dTLSCerPaths[1], dTLSCerPaths[2]
+		client, err := docker.NewTLSClient(dockerEP, cert, key, ca)
 		if err != nil {
 			return err
 		}
 		return fn(client)
 	}
-	client, err := docker.NewClient(dockerEndPoint)
+	// Use plain HTTP client
+	client, err := docker.NewClient(dockerEP)
 	if err != nil {
 		return err
 	}
 	return fn(client)
 }
 
-// DockerImgIsPresent checks if the image is present on the Docker host
-func IsDockerImgPresent(img *resource.DockerImg) (bool, error) {
-	err := withDockerClient(func(client *docker.Client) error {
+// Looks up *docker.APIImage which matches *resource.DockerImg passed as paramter and
+// calls an anonymous func on it
+func withDockerAPIImage(i *resource.DockerImg, fn func(*docker.APIImages) error) error {
+	return withDockerClient(func(client *docker.Client) error {
 		imgs, err := client.ListImages(docker.ListImagesOptions{All: false})
 		if err != nil {
 			return err
 		}
-		for _, i := range imgs {
-			for _, repoTag := range i.RepoTags {
+		for _, img := range imgs {
+			for _, repoTag := range img.RepoTags {
 				repo, tag := docker.ParseRepositoryTag(repoTag)
-				if repo == img.Repo {
-					// TODO: worth doing switch-case?
-					if img.Tag == "" {
+				if repo == i.Repo {
+					if i.Tag == "" {
 						if tag == "latest" {
-							return nil
+							return fn(&img)
 						}
 					}
-					if tag == img.Tag {
-						return nil
+					if tag == i.Tag {
+						return fn(&img)
 					}
 				}
 			}
 		}
-		return fmt.Errorf("Image %s not found", img)
+		return fmt.Errorf("Image %s not found", i)
+	})
+}
+
+// Looks up *docker.APIContainer that matches *resource.DockerContainer passed as parameter and
+// calls an anonymous func on it
+func withDockerAPIContainer(c *resource.DockerContainer, fn func(*docker.APIContainers) error) error {
+	return withDockerClient(func(client *docker.Client) error {
+		containers, err := client.ListContainers(docker.ListContainersOptions{All: true})
+		if err != nil {
+			return err
+		}
+		for _, container := range containers {
+			// Container ID has the highest priority
+			if c.ID != "" && container.ID == c.ID {
+				return fn(&container)
+			}
+			if c.Name != "" {
+				if !strings.HasPrefix(c.Name, "/") {
+					c.Name = "/" + c.Name
+				}
+				for _, name := range container.Names {
+					if c.Name == name {
+						return fn(&container)
+					}
+				}
+			}
+		}
+		return fmt.Errorf("Container %s not found", c)
+	})
+}
+
+// IsDockerImgPresent checks if the image is present on the Docker host
+// It returns true if the images is present or error
+func IsDockerImgPresent(img *resource.DockerImg) (bool, error) {
+	err := withDockerAPIImage(img, func(apiImg *docker.APIImages) error {
+		return nil
 	})
 	if err != nil {
 		return false, err
@@ -74,29 +130,10 @@ func IsDockerImgPresent(img *resource.DockerImg) (bool, error) {
 }
 
 // IsDockerContainerPresent checks if docker container is present on the host
+// It returns true if the images is present or error
 func IsDockerContainerPresent(c *resource.DockerContainer) (bool, error) {
-	err := withDockerClient(func(client *docker.Client) error {
-		containers, err := client.ListContainers(docker.ListContainersOptions{All: true})
-		if err != nil {
-			return err
-		}
-		for _, container := range containers {
-			// Container ID has the highest priority
-			if c.ID != "" && container.ID == c.ID {
-				return nil
-			}
-			if c.Name != "" {
-				if !strings.HasPrefix(c.Name, "/") {
-					c.Name = "/" + c.Name
-				}
-				for _, name := range container.Names {
-					if c.Name == name {
-						return nil
-					}
-				}
-			}
-		}
-		return fmt.Errorf("Container %s not found", c)
+	err := withDockerAPIContainer(c, func(apiContainer *docker.APIContainers) error {
+		return nil
 	})
 	if err != nil {
 		return false, err
--- resource/docker.go
+++ resource/docker.go
@@ -3,7 +3,7 @@ package resource
 import "fmt"
 
 // DockerImg defines Docker image
-// It embeds pointer to docker.Image
+// It references its image Repo and its Tag
 type DockerImg struct {
 	// Docker Repo
 	Repo string
@@ -11,13 +11,13 @@ type DockerImg struct {
 	Tag string
 }
 
-// Implement stringer interface
+// Implements stringer interface
 func (d *DockerImg) String() string {
 	return fmt.Sprintf("[Docker Image] Name: %s, Tag: %s", d.Repo, d.Tag)
 }
 
 // DockerContainer defines Docker container
-// It embeds pointer to docker container
+// It has ID and an Name
 type DockerContainer struct {
 	// Docker container Id
 	ID string
@@ -25,7 +25,7 @@ type DockerContainer struct {
 	Name string
 }
 
-// Implement stringer interface
+// Implements stringer interface
 func (d *DockerContainer) String() (c string) {
 	return fmt.Sprintf("[Docker Container] Name: %s, ID: %s", d.Name, d.ID)
 }
